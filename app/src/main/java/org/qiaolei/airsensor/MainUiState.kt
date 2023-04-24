ad4959063408233874fa2df3d8df82349019e9ac
matt <flowingfirefly@foxmail.com>
2023-04-25 00:28:42 +0800

    fix bug

--- app/src/main/java/org/qiaolei/airsensor/MainUiState.kt
+++ app/src/main/java/org/qiaolei/airsensor/MainUiState.kt
@@ -1,8 +1,10 @@
 package org.qiaolei.airsensor
 
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.snapshots.SnapshotStateList
 import org.qiaolei.airsensor.data.DeviceModel
 
 data class MainUiState(
     val isScanning: Boolean = false,
-    val devices: MutableList<DeviceModel> = mutableListOf()
+    val devices: SnapshotStateList<DeviceModel> = mutableStateListOf()
 )
